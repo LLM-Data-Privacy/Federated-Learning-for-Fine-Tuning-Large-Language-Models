335e061a97df694dde818d5408cc081d70aa422b
Haoran-Bao <albert_bao@hotmail.com>
2024-07-30 11:42:58 -0400

    write a script for superlink

---
+++ run-superlink.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=flower_simulation
+#SBATCH --nodes=2
+#SBATCH --ntasks=6  # 1 for superlink, 1 for server, 4 for clients
+#SBATCH --time=01:30:00
+#SBATCH --partition=short
+
+module load gcc/8.4.0/1  cuda/10.2
+module load openmpi/4.0.3/1
+module load python
+
+# Run flower-superlink
+mpirun -np 1 flower-superlink --insecure > superlink.log 2>&1 &
+
+# Run flower-client-app for 4 clients
+for i in {0..3}; do
+  mpirun -np 1 flower-client-app client:app --insecure > client_$i.log 2>&1 &
+done
+
+# Run flower-server-app
+mpirun -np 1 flower-server-app server:app --insecure > server.log 2>&1 &
+
+wait
